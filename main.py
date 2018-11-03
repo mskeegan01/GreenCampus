58bb0e7be251646421abebda28688f58f7c6bbe7
Darjusch <Hans@gmx.de>
2018-11-03 16:13:52 +0100

    Working database

--- main.py
+++ main.py
@@ -1,33 +1,55 @@
 import serial
 import sqlite3
+import time
+import calendar
 
+#Database is created 
 db = sqlite3.connect('mydb')
 cursor = db.cursor()
+
 #cursor.execute(
 #    "CREATE TABLE plants(id INTEGER PRIMARY KEY, name TEXT, temperature FLOAT, moisture INT, light INT)"
 #)
 #db.commit()
 
-id1 = '0'
-name1 = "Chilli"
-temperature1 = '30.69'
-moisture1 = '63'
-light1 = '15'
+#Create timestamp column in database
+#cursor.execute(
+#    "ALTER TABLE plants ADD COLUMN timestamp TEXT"
+#)
+#db.commit()
 
-id2 = '1'
-name2 = "Basil"
-temperature2 = '25.33'
-moisture2 = '34'
-light2 = '25'
+data = {}
+i = 0
 
-#cursor.execute('''INSERT INTO plants(id, name, temperature, moisture, light)
-#                  VALUES(?,?,?,?,?)''', (id1,name1, temperature1, moisture1, light1))
-print("XYZ")
+#Connection to the Arduino
+serialConnection = serial.Serial('/dev/cu.usbmodem1421')
+#Transforms received data into readable format for the database and adds a timestamp
+lineCount = 4
+while i < lineCount:
+    x = serialConnection.readline()
 
+    #Replacing unnessesary characters
+    x = x.decode('UTF-8')
+    if ':' in x:
+        split = x.split(':')
+        split[1] = split[1].replace('\r\n', '')
+        split[1] = split[1].replace(' ', '')
+        data.update({split[0]: split[1]})
+print(data)
+#Create timestamp when data is transmitted
+
+# 2018-10-11 12:23:12
+ts = time.gmtime()
+#print(time.strftime("%Y-%m-%d %H:%M:%S", ts))
+#Inserting the data into the table 
+#Deleted id from values to check for autoincrement 
+cursor.execute('''INSERT INTO plants(name, temperature, moisture, light, timestamp)
+                  VALUES(?, ?, ?, ?, ?)''', ( 'Pepper', data['Temperature'], data['Moisture'], data['Lightintensity'],time.strftime("%Y-%m-%d %H:%M:%S", ts))) 
 db.commit()
+#Displaying all data in the database 
 
-ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
-while True:
-    x = ser.readline()
-    print(x.decode('UTF-8')[:-2])
+cursor.execute('''SELECT * FROM plants ''')
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
 db.close()
